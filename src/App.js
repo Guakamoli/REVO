30a621534544b261ba66bcb1fe2fb2a478691eb2
xujalg <1027754098@qq.com>
2022-05-22 12:40:32 +0800

    feat:精修

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import ReactAudioPlayer from 'react-audio-player';
 function App() {
   const [mute, setMute] = useState(true);
   const [rootHight, setRootHight] = useState(800);
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
   const [videoUrl, setVideoUrl] = useState("./image/backgroundVideo.mp4");
+  const [loadingUrl, setLoadingUrl] = useState({
+    video:"./image/loading.mp4",
+    image: './image/loadingFirst.jpeg'
+  })
   const [loading, setLoading] = useState(true);
   const [interval, setInterval] = useState(1000);
   const TWITTER_URL = "https://twitter.com/R3VO_XYZ";
@@ -23,6 +27,11 @@ function App() {
     setIsMobile(isPhone);
     if (isPhone) {
       setVideoUrl("./image/backgroundVideo_mobile.mp4");
+      setLoadingUrl({
+        video:"./image/loading_mobile.mp4",
+        image: './image/loadingMobileFirst.jpeg'
+        
+      })
       setRootHight(document.documentElement?.clientHeight || window.innerHeight);
     }
     if(!isPhone){
@@ -46,7 +55,9 @@ function App() {
     console.info('document.readyState',document.readyState)
     if (document.readyState === 'complete') {
       setInterval(null)
-      setLoading(false)
+      setTimeout(() =>{
+        setLoading(false)
+      },4000)
       }
   }, interval);
   const myRef = useRef();
@@ -67,95 +78,103 @@ function App() {
   },[mute])
   return (
     <div className={styles.root} style={{ height:isMobile ? rootHight :'100vh' }}>
-      <div className={styles.header}>
-        <img src="./image/logo@2x.png" className={styles.logo}></img>
-        {/* <img src="./image/joinBtn@2x.png" className={styles.joinBtn}></img> */}
-      </div>
-      <div className={styles.content}>
-        <>
-        {
-          loading ?
-          <></>
-          // <img src="./image/joinBtn.png"  className={styles.bgvid} style={{zIndex:999}}></img>
-          
-          :
+      {
+         <div style={{ width:"100%",height:"100%", display:loading ? 'flex' :"none" ,justifyContent:'center',alignItems:'center' }} >
+            <video
+              autoPlay
+              muted={true}
+              loop="loop"
+              playsInline
+              x5-video-player-type="h5"
+              poster={'./image/loadingFirst.jpeg'}
+              className={styles.loading}
+              preload="none"
+              src={'./image/loading.mp4'}  type="video/mp4" 
+              // style={{display:!isMobile ?"flex" :'none'}}
+            >
+            </video>
+          </div>
+      }
+   
+        <div style={{ width:"100%",height:"100%", display:!loading ? 'flex' : 'none' }}>
+        <div className={styles.header}>
+      <img src="./image/logo@2x.png" className={styles.logo}></img>
+      {/* <img src="./image/joinBtn@2x.png" className={styles.joinBtn}></img> */}
+        </div>
+        <div className={styles.content}>
           <>
-          <video
-            autoPlay
-            muted={true}
-            loop="loop"
-            playsInline
-            x5-video-player-type="h5"
-            // poster="./image/videoFirst.png"
-            className={styles.bgvid}
-          
-          >
-            <source src={videoUrl} type="video/mp4" />
-          </video>
-        <ReactAudioPlayer
-            src="./image/backgroundMusic.mp3"
-            controls
-            loop
-            style={{position: "absolute",opacity:0}}
-            ref={myRef}
-          />
+            <>
+              <video
+                autoPlay
+                muted={true}
+                loop="loop"
+                playsInline
+                x5-video-player-type="h5"
+                poster="./image/videoFirst.png"
+                className={styles.bgvid}
+              
+              >
+                <source src={videoUrl} type="video/mp4" />
+              </video>
+              <ReactAudioPlayer
+                src="./image/backgroundMusic.mp3"
+                controls
+                loop
+                style={{position: "absolute",opacity:0}}
+                ref={myRef}
+              />
+            </>
+            <div className={styles.mask}></div>
           </>
-        }
-          
-          <div className={styles.mask}></div>
-        </>
-
-        <div className={styles.title}></div>
-
-        {/* background: url("../../public/image/title@2x.png"); */}
-        <div className={styles.titleFont}>
-          <text style={{ width: 606 }}>
-            REVO IS A WEB 3.0 VIDEO SHARING SOCIAL NETWORKING SERVICE THAT IS
-            OWNED BY COMMUNITY. 100% OF OUR INCOME WILL BE GIVEN BACK TO OUR
-            USERS AND CREATORS.
-          </text>
-        </div>
-        <img src="./image/joinUs@2x.png" className={styles.joinUs} onClick={()=>{jumpUrl(DISCORD_URL)}}></img>
-      </div>
-
-      <div className={styles.footer}>
-        <div className={styles.footerLogo}>
-          <img
-            src="./image/twitterLogo@2x.png"
-            className={styles.twitterLogo}
-            onClick={()=>{jumpUrl(TWITTER_URL)}}
-          ></img>
-          <img
-            src="./image/cameoLogo@2x.png"
-            className={styles.cameoLogo}
-            onClick={()=>{jumpUrl(CAME_URL)}}
-          ></img>
-            <div onClick={musicPlaying} style={{opacity: isMobile ? 1 : 0}} >
-            <img src="./image/mute@2x.png" className={styles.muteImg} style={{display:mute ? 'flex' :'none'}}/>
-     
-     <img src="./image/playing@2x.gif" className={styles.playingImg} style={{display: !mute ? 'flex' :'none'}}/>
-         </div>
-        </div>
-
-        <div className={styles.footerBacked}>
-          <div
-            className={styles.musicControl}
-            onClick={musicPlaying}
-            style={{ display: isMobile ? 'none':"flex",alignItems : mute ? 'center':'flex-end' }}
-          >
-            <text className={styles.musicControlfont}>Music Control</text>
-            <img src="./image/mute@2x.png" className={styles.muteImg} style={{display:mute ? 'flex' :'none'}}/>
-     
-     <img src="./image/playing@2x.gif" className={styles.playingImg} style={{display: !mute ? 'flex' :'none'}}/>
+          <div className={styles.title}></div>
+          <div className={styles.titleFont}>
+            <text style={{ width: 606 }}>
+              REVO IS A WEB 3.0 VIDEO SHARING SOCIAL NETWORKING SERVICE THAT IS
+              OWNED BY COMMUNITY. 100% OF OUR INCOME WILL BE GIVEN BACK TO OUR
+              USERS AND CREATORS.
+            </text>
           </div>
-          <div className={styles.backedControl}>
-            <text className={styles.backedfont}>Backed by</text>
-            <img src="./image/SIG@2x.png" className={styles.SIG}
-             onClick={()=>{jumpUrl(SIG)}}
+          <img src="./image/joinUs@2x.png" className={styles.joinUs} onClick={()=>{jumpUrl(DISCORD_URL)}}></img>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.footerLogo}>
+            <img
+              src="./image/twitterLogo@2x.png"
+              className={styles.twitterLogo}
+              onClick={()=>{jumpUrl(TWITTER_URL)}}
             ></img>
+            <img
+              src="./image/cameoLogo@2x.png"
+              className={styles.cameoLogo}
+              onClick={()=>{jumpUrl(CAME_URL)}}
+            ></img>
+              <div onClick={musicPlaying} style={{opacity: isMobile ? 1 : 0}} >
+              <img src="./image/mute@2x.png" className={styles.muteImg} style={{display:mute ? 'flex' :'none'}}/>
+      
+      <img src="./image/playing@2x.gif" className={styles.playingImg} style={{display: !mute ? 'flex' :'none'}}/>
+          </div>
+          </div>
+
+          <div className={styles.footerBacked}>
+            <div
+              className={styles.musicControl}
+              onClick={musicPlaying}
+              style={{ display: isMobile ? 'none':"flex",alignItems : mute ? 'center':'flex-end' }}
+            >
+              <text className={styles.musicControlfont}>Music Control</text>
+              <img src="./image/mute@2x.png" className={styles.muteImg} style={{display:mute ? 'flex' :'none'}}/>
+      
+      <img src="./image/playing@2x.gif" className={styles.playingImg} style={{display: !mute ? 'flex' :'none'}}/>
+            </div>
+            <div className={styles.backedControl}>
+              <text className={styles.backedfont}>Backed by</text>
+              <img src="./image/SIG@2x.png" className={styles.SIG}
+              onClick={()=>{jumpUrl(SIG)}}
+              ></img>
+            </div>
           </div>
         </div>
-      </div>
+        </div>      
     </div>
   );
 }
